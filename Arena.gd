4f24379c52045f2a7fd6edaee69a68b40a80d032
Sven Wenzel <wenzel.s.developer@gmail.com>
2021-06-12 13:02:58 +0200

    Added wall segment spawn

--- Arena.gd
+++ Arena.gd
@@ -5,14 +5,14 @@ var WallSegment = preload("WallSegment.tscn")
 export(NodePath) var player_node_path
 var player
 
-var width = 500
-var arenaChunkHeight = 500
-var lastChunkIndex = -1
+var arena_width = 500
+var arena_chunk_height = 50
+var last_chunk_index = -1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node(player_node_path)
+	player = get_node(player_node_path).camera
 	
 func _init():
 	# Draw initial arena tiles
@@ -22,18 +22,25 @@ func _init():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Check where player is located, create new chunks
-	var playerChunkIndex = int(player.position.y) / arenaChunkHeight
+	var player_chunk_index = int(-player.position.y) / arena_chunk_height
 	
 	# Create new chunks if player is close enough to current last chunk
-	while playerChunkIndex > lastChunkIndex - 3:
-		lastChunkIndex += 1
-		createNewChunk(lastChunkIndex)
+	while player_chunk_index > last_chunk_index - 3:
+		last_chunk_index += 1
+		createNewChunk(last_chunk_index)
 		
-	pass
 
 func createNewChunk(index):
+	var position = Vector2(-arena_width, -index * arena_chunk_height)
+	var extent = Vector2(20, arena_chunk_height)
+	
+	spawnWallSegment(position, extent)
+	position.x *= -1
+	spawnWallSegment(position, extent)
+
+func spawnWallSegment(position, extent):
 	var new_wall_segment = WallSegment.instance()
 	add_child(new_wall_segment)
 	
-	new_wall_segment.position = Vector2(0, -index * arenaChunkHeight)
-	new_wall_segment.extents.y = arenaChunkHeight
+	new_wall_segment.position = position
+	new_wall_segment.extents = extent
